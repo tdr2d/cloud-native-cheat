ca32308a78b6150d6ee2d35e3712d5c1ffbc7453
Thomas Webber <webber.thomas78@gmail.com>
2020-08-02 22:54:45 +0200

    updated bootable_usb.sh

--- sys/bootable_usb.sh
+++ sys/bootable_usb.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 
 cat << EOF
+Alternatives: https://www.balena.io/etcher/
 Known errors:
 
 dd: /dev/rdiskN: Resource busy -> need to unmount disk first
@@ -11,6 +12,7 @@ dd: bs: illegal numeric value -> change bs=1m to bs=1M
 dd: /dev/rdiskN: Operation not permitted -> disable SIP before continuing
 dd: /dev/rdiskN: Permission denied -> erase first with 
     sudo diskutil partitionDisk /dev/diskN 1 MBR "Free Space" "%noformat%" 100%
+
 
 EOF
 
@@ -22,12 +24,11 @@ bootable_usb() {
     diskutil list
 
     printf "Choose your disk (eg /dev/disk2) : " && read disk
-    diskutil unmountDisk $disk
     printf "\nWould you like to format $disk ? (y / n)" && read confirm_format
-    if [ "confirm_format" == "y" ]; then
-        sudo diskutil partitionDisk $disk 1 MBR "Free Space" "%noformat%" 100%
-        echo "$disk formated"
+    if [ "$confirm_format" == "y" ]; then
+        diskutil eraseDisk FAT32 UNTITLED MBRFormat $disk
     fi
+    diskutil unmountDisk $disk
     printf "\nChoose your img path (eg: /home/thomas/Download/myos.img)" && read img_path
     printf "\nConfirm writing $img_path to $disk ? (y / n)" && read confirm
     
@@ -38,6 +39,7 @@ bootable_usb() {
     fi
 
     sudo diskutil eject $disk
+    echo "Ejected $disk"
 }
 
 bootable_usb
