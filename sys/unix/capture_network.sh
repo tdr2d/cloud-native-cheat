cbc03394c0d7ce4a38eb177d26fd5526d1f338be
Thomas Webber <webber.thomas78@gmail.com>
2020-09-21 17:10:27 +0200

    added dhcpd conf for manager - dnsmasq doest work...

---
+++ sys/unix/capture_network.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# On centos: yum install wireshark -y
+
+if [ "$#" -ne 1 ]; then
+    echo "Incorect args. Usage: $0 <interface>"
+    exit 1
+fi
+
+sudo tshark -i $1 -T fields \
+-e eth.src \
+-e ip.src \
+-e ip.dst \
+-e frame.protocols \
+-E header=y
--- vmware/env.sh
+++ vmware/env.sh
@@ -1,2 +1,4 @@
+source ~/.env.sh
 export TF_VAR_vsphere_user="$VSPHERE_USER"
 export TF_VAR_vsphere_password="$VSPHERE_PASSWORD"
+export ANSIBLE_HOST_KEY_CHECKING=False
