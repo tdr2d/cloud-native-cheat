696b5f934464556bcfdec5ac95053d091e9a0e28
Thomas Webber <webber.thomas78@gmail.com>
2020-10-07 16:47:04 +0200

    refactored vmware dir, added templates dir

--- sys/unix/install_hashicorp.sh
+++ sys/unix/install_hashicorp.sh
@@ -5,17 +5,23 @@ set -eu
 # Usage ./install_hashicorp.sh terraform
 # arg1: terraform, packer
 
+readonly system="$(uname -s | awk '{print tolower($0)}')"
+readonly binary=$1
+
 cd /tmp
 if [ "$1" = "terraform" ]; then
   readonly VERSION="0.13.2"
-  curl -LO https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip
+  curl -LO https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_${system}_amd64.zip
   unzip /tmp/terraform_${VERSION}_linux_amd64.zip
   sudo mv /tmp/terraform /usr/local/bin
   rm -f terraform_${VERSION}_linux_amd64.zip
 elif [ "$1" = "packer" ]; then
   readonly VERSION="1.6.2"
-  curl -LO https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip && \
+  curl -LO https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_${system}_amd64.zip && \
   unzip packer_${VERSION}_linux_amd64.zip && \
   sudo mv /tmp/packer /usr/local/bin && \
   rm -r packer_${VERSION}_linux_amd64.zip
 fi
+
+
+
---
+++ vmware/templates/rhel/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+readonly tmpdir="/tmp/templates/rhel"
+mkdir -p $tmpdir
+
+ansible localhost -m template -a "src=packer.json dest=${tmpdir}/packer.json" -e @../../vars.yml \
+    -e template_ip=192.168.95.242 \
+    -e template_hostname=t-rhel-7-8.adlere.priv \
+    -e kernet_ssh_pass=$KERNET_SSH_PASS \
+    -e work_dir=$tmpdir
+
+ansible localhost -m template -a "src=ks.cfg dest=${tmpdir}/ks.cfg" -e @../../vars.yml \
+    -e template_ip=192.168.95.242 \
+    -e template_hostname=t-rhel-7-8.adlere.priv \
+    -e kernet_ssh_pass=$KERNET_SSH_PASS \
+    -e work_dir=$tmpdir
+
+cd $tmpdir && packer build packer.json
