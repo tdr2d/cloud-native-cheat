61829140c684aa1f5c3b1a19b5c46baf669292c9
tdr2d <28861767+tdr2d@users.noreply.github.com>
2024-07-25 17:59:35 +0200

    updated install python script

--- sys/unix/install_python.sh
+++ sys/unix/install_python.sh
@@ -4,12 +4,12 @@ set -eu
 
 readonly VERSION="${1-3.8.5}"
 
-# usage: ./py.sh 3.6.9
+# usage: sudo ./py.sh 3.12.4
 
 
 # Prereq
 yum install @development -y || apt install build-essential -y
-yum install zlib-devel -y   || apt-get install zlib1g-dev -y
+yum install zlib-devel -y   || apt-get install zlib1g-dev libssl-dev -y
 
 # Build python
 curl -LO https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tar.xz
