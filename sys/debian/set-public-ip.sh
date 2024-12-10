a6055088a1db3e474b43ede081aac6c1227118c5
tdr2d <28861767+tdr2d@users.noreply.github.com>
2024-12-10 09:53:33 +0100

    added sys/debian/set-public-ip.sh to configure ip with netplan

---
+++ sys/debian/set-public-ip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eo pipefail
+
+ADDITIONAL_IP=${1}
+OUTPUT_FILE='/etc/netplan/51-cloud-init.yaml'
+
+if [ -z ${ADDITIONAL_IP} ]; then
+    echo "Wrong Usage: "
+    echo "./set-public-ip.sh <my.ip.v.4>"
+    exit 1
+fi
+
+rm -f $OUTPUT_FILE
+cat << EOF > $OUTPUT_FILE
+network:
+  version: 2
+  ethernets:
+    ens3:
+      dhcp4: true
+      addresses:
+        - ${ADDITIONAL_IP}/32
+EOF
+# sudo chmod 600 -R /etc/netplan/
+
+echo "Writing netplan configution ${OUTPUT_FILE}"
+cat $OUTPUT_FILE
+echo "Applying configuration"
+
+# sudo netplan try --timeout 3
+sudo netplan apply
